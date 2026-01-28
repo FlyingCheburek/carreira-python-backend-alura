d54fbd87d3f0f7b32fb70f9032981811adba7bb5
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2026-01-27 23:01:08 -0300

    feat: Listagem de funcionalidades (menu)
    O usuário poderá escolher uma opção para executar determinada ação.

---
+++ Atividades e Código/Nível Base/1. Primeira Aplicação em Python/Sabor Express/app.py
@@ -0,0 +1,10 @@
+print("\n=== SABOR EXPRESS ===\n")
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+opcao_escolhida: str = input("Escolha uma opção: ")
+
+print("\nVocê escolheu a opção: ", opcao_escolhida)
