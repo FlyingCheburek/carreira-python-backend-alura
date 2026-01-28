0fbf169f0c38e53d71739bde3bbc9a73ebe4be8b
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2026-01-27 23:21:49 -0300

    feat: exercícios com a função print()
    Testando diferentes métodos para manipulação de strings

---
+++ Atividades e Código/Nível Base/1. Primeira Aplicação em Python/Exercício função PRINT/app.py
@@ -0,0 +1,20 @@
+print('\nPython na Escola de Programação da Alura!\n')
+
+nome = "Thomas Pinheiro"
+idade = 20
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print("""
+      A
+      L
+      U
+      R
+      A
+      """)
+
+PI = 3.14159
+print(f'Pi arredondado para 2 casas decimais é: {PI:.2f}')
+# print('Pi arredondado para 2 casas decimais é: ', round(PI, 2))
+
+
+
